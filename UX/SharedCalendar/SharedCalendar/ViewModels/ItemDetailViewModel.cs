6f35e43e0d370ea124e0bc6a75116e622f0a90e0
ksummers112 <43285227+ksummers112@users.noreply.github.com>
2018-09-14 20:29:30 -0500

    First commit of project. This just has the initial projects in it.

---
+++ UX/SharedCalendar/SharedCalendar/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SharedCalendar.Models;
+
+namespace SharedCalendar.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
